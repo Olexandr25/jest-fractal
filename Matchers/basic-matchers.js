62e1613c24e48ebfc543db3b86a7d12427b56e95
Olexandr25 <oleksandrvoichyshyn@gmail.com>
2021-08-11 19:03:43 +0300

    Made all Truthiness matchers

--- Matchers/basic-matchers.js
+++ Matchers/basic-matchers.js
@@ -1,28 +1,55 @@
 // Connection of describe-test.js
 const { describe, test } = require("../describe-test.js");
 
+// Connection Lodash library
+var _ = require('lodash');
+
 // Function of basic equal
-function expect(value) {
+function expect(exp) {
     return {
         // Check is this x === y
-        toBe: exp => {
-            if (value === exp)
+        toBe: value => {
+            if (exp === value)
                 console.log('\x1b[32m', 'Success');
             else
                 console.log('\x1b[31m', 'Failed');
         },
         // Check is not this x !== y
-        toBeNot: exp => {
-            if (value !== exp)
+        toBeNot: value => {
+            if (exp !== value)
+                console.log('\x1b[32m', 'Success');
+            else
+                console.log('\x1b[31m', 'Failed');
+        },
+        // Check Deep equality
+        // ! Doesn't work
+        // toEqual: value => {
+        //     const isArrayEqual = (value, exp) => _.isEmpty(_.xorWith(value, exp, _.isEqual));
+
+        //     if (isArrayEqual)
+        //         console.log('\x1b[32m', 'Success');
+        //     else
+        //         console.log('\x1b[31m', 'Failed');
+        // },
+
+        toEqualConvertToString: value => {
+            // compare lengths - can save a lot of time 
+            let result = value.length === exp.length && value.every((v, index) => v === exp[index])
+            if (result)
                 console.log('\x1b[32m', 'Success');
             else
                 console.log('\x1b[31m', 'Failed');
         }
+
     }
 }
 
 // Sum of a + b
 const sum = (a, b) => a + b;
+
+const a = [1, 2, 3];
+const b = [4, 5, 6];
+const c = [1, 2, 3];
 
 // Call test
 // First global describe
@@ -30,6 +57,6 @@ describe('Native matcher:', () => {
     // Second describe of tests
     test('sum values', () => {
         expect(sum(41, 1)).toBe(42)
-        expect(sum(1, 4)).toBeNot(5)
+        expect(sum(1, 4)).toBeNot(6)
     })
 })
