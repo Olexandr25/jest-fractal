9838e6196cc95f2d8431911d37990ccd37a992fc
Olexandr25 <oleksandrvoichyshyn@gmail.com>
2021-08-12 15:30:11 +0300

    Rewrite my own code for better reading, and made last basic test

---
+++ Matchers/help.js
@@ -0,0 +1,22 @@
+// Connection of describe-test.js
+const { describe, test } = require("../describe-test.js");
+
+// Connection Lodash library
+var _ = require('lodash');
+
+function expect(exp) {
+    return {        
+        stringMatching: value => exp.match(value),
+        stringContaining: value => exp.includes(value),
+        toEqual: (value_1, value_2) => _.isEqual(value_1, value_2)
+    }
+}
+
+
+// First describe
+describe('String matcher:', () => {
+    // Second describe of tests
+    test('values', () => {
+        expect('pizzaa').stringContaining('zz');
+    })
+})
