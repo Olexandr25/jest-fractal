452553b134ed97c82f9e8339414d5bd1b65e0856
whoiam460 <whoiam@i.ua>
2021-08-13 16:28:03 +0300

    commit

--- src/Matchers/basic-matchers.js
+++ src/Matchers/basic-matchers.js
@@ -68,24 +68,24 @@ function expect(exp) {
         toMatchReg: value => (exp.match(value)) ? console.log('\x1b[32m', 'Success') : console.log('\x1b[31m', 'Failed')
     }
 }
+module.exports = { expect };
 
 // Sum of a + b
-const sum = (a, b) => a + b;
+// const sum = (a, b) => a + b;
 
-const a = [1, 2, 3];
-const b = [4, 5, 6];
-const c = [1, 2, 3];
+// const a = [1, 2, 3];
+// const b = [4, 5, 6];
+// const c = [1, 2, 3];
 
 // Call test
 // First global describe
-describe('Native matcher:', () => {
-    // Second describe of tests
-    test('values', () => {
-        expect(sum(41, 1)).toBe(42);
-        expect(sum(1, 4)).toBeNot(6);
-        expect(a).toEqual(c);
-        expect({a: undefined, b: 2}).toEqual({b: 2});
-    })
-})
+// describe('Native matcher:', () => {
+//     // Second describe of tests
+//     test('values', () => {
+//         expect(sum(41, 1)).toBe(42);
+//         expect(sum(1, 4)).toBeNot(6);
+//         expect(a).toEqual(c);
+//         expect({a: undefined, b: 2}).toEqual({b: 2});
+//     })
+// })
 
-module.exports = { expect };
--- test/basic.test.js
+++ test/basic.test.js
@@ -1,21 +1,14 @@
 const { describe, test } = require("../src/describe-test");
-const {expect} = require("../src/Matchers/basic-matchers");
+const { expect } = require("../src/Matchers/basic-matchers.js");
 
-
-
-
-// const sum = (a, b) => a + b; 
-
-
-// describe('Native matcher:', () => {
-//     // Second describe of tests
-//     test('values', () => {
-//         expect(sum(41, 1)).toBe(42);
-//         expect(sum(1, 4)).toBeNot(6);
-//         expect(a).toEqual(c);
-//         expect({a: undefined, b: 2}).toEqual({b: 2});
-//     })
-// })
-
-
-console.log("kklvflk")
+// Call test
+// First global describe
+describe('Native matcher:', () => {
+    // Second describe of tests
+    test('values', () => {
+        expect(2).toBe(2);
+        expect(42).toBe(42);
+        expect(5).toBeNot(6);
+        expect({a: undefined, b: 2}).toEqual({b: 2});
+    })
+})
